7465c38a746dce72537b86f81ce6e533a61403b6
Kimberly Nicholls <kielni@yahoo.com>
2017-07-07 21:35:35 -0700

    query by source instead of by week

--- server/fcc_analysis/lambda.py
+++ server/fcc_analysis/lambda.py
@@ -2,13 +2,21 @@ import os
 
 from elasticsearch import Elasticsearch
 
-def query_positive_by_date(event=None, context=None):
+import tags
+
+def query_by_source(event=None, context=None):
+    es = Elasticsearch(os.environ['ES_ENDPOINT'])
+    # positive
     query = {
       "size": 0,
       "query": {
         "bool": {
-          "minimum_should_match": 1,
           "should": [
+            {
+              "term": {
+                "analysis.sentiment_manual": True
+              }
+            },
             {
               "term": {
                 "analysis.titleii": True
@@ -23,23 +31,80 @@ def query_positive_by_date(event=None, context=None):
         }
       },
       "aggs": {
-        "by_week": {
-          "date_histogram": {
-            "field": "date_received",
-            "interval": "week"
+        "source": {
+          "terms": {
+            "field": "analysis.source"
+          }
+        },
+        "sigterms": {
+          "terms": {
+            "field": "analysis.sentiment_sig_terms_ordered"
           }
         }
       }
     }
-    es = Elasticsearch(os.environ['ES_ENDPOINT'])
+    query['query']['bool']['should'].append({
+        'terms': {'analysis.source': tags.sources['positive']}
+    })
     resp = es.search(index='fcc-comments', body=query)
-    by_week = {}
-    for week in resp['aggregations']['by_week']['buckets']:
-        by_week[week['key_as_string']] = week['doc_count']
-    return {
-        'total': resp['hits']['total'],
-        'by_week': by_week
+    by_source = {}
+    for src in resp['aggregations']['source']['buckets']:
+        if src['key'] == 'unknown':
+            continue
+        by_source[src['key']] = src['doc_count']
+    by_source['sig_terms'] = resp['aggregations']['sigterms']['buckets'][0]['doc_count']
+    rval = {
+        'total_positive': resp['hits']['total'],
+        'positive_by_source': by_source
     }
 
+    # negative
+    query = {
+      "size": 0,
+      "query": {
+        "bool": {
+          "should": [
+            {
+              "term": {
+                "analysis.sentiment_manual": False
+              }
+            },
+            {
+              "term": {
+                "analysis.titleii": False
+              }
+            },
+            {
+              "term": {
+                "analysis.sentiment_sig_terms_ordered": False
+              }
+            }
+          ]
+        }
+      },
+      "aggs": {
+        "source": {
+          "terms": {
+            "field": "analysis.source"
+          }
+        }
+      }
+    }
+    query['query']['bool']['should'].append({
+        'terms': {'analysis.source': tags.sources['negative']}
+    })
+    resp = es.search(index='fcc-comments', body=query)
+    by_source = {}
+    for src in resp['aggregations']['source']['buckets']:
+        if src['key'] == 'unknown':
+            continue
+        by_source[src['key']] = src['doc_count']
+    rval.update({
+        'total_negative': resp['hits']['total'],
+        'negative_by_source': by_source
+    })
+    return rval
+
+
 if __name__ == '__main__':
-    print(query_positive_by_date())
+    print(query_by_source())
