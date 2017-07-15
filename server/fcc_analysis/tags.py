4e96c7144fd813d735b5a42302f3eec53c8ce126
Kimberly Nicholls <kielni@yahoo.com>
2017-07-14 17:48:12 -0700

    add positive clusters

--- server/fcc_analysis/tags.py
+++ server/fcc_analysis/tags.py
@@ -1,4 +1,31 @@
 sources = {
-    'positive': ["form.battleforthenet", "reddit.technology", "blog.venturebeat", "bot.internetuser", "form.dearfcc", "bot.illogically-named", "form.signforgood", "form.freepress", "form.demandprogress", "bot.maga", "bot.dissapear", "bot.telecommunications_act", "bot.thenewsearchengine"],
-    'negative': ["bot.american-jobs", "bot.unprecedented", "bot.outraged", "form.diminished-investment", "form.freeourinternet", "form.fwact", "form.tpa", "bot.best_cop", "bot.economics_free_zone", "bot.corrupt_result", "bot.recursive"]
+    'positive': [
+        "form.battleforthenet", "reddit.technology", "blog.venturebeat",
+        "bot.internetuser", "form.dearfcc", "bot.illogically-named", "form.signforgood",
+        "form.freepress", "form.demandprogress", "bot.maga", "bot.dissapear",
+        "bot.telecommunications_act", "bot.thenewsearchengine"],
+    'negative': [
+        "bot.american-jobs", "bot.unprecedented", "bot.outraged",
+        "form.diminished-investment", "form.freeourinternet", "form.fwact",
+        "form.tpa", "bot.best_cop", "bot.economics_free_zone", "bot.corrupt_result",
+        "bot.recursive"]
+}
+sources = {
+    'positive': [
+        "form.battleforthenet", "reddit.technology", "blog.venturebeat", "bot.internetuser",
+        "form.dearfcc", "bot.illogically-named", "form.signforgood", "form.freepress",
+        "form.demandprogress", "bot.maga", "bot.dissapear", "bot.telecommunications_act",
+        "bot.thenewsearchengine", "es_terms_positive"],
+    'negative': [
+        "bot.american-jobs", "bot.unprecedented", "bot.outraged",
+        "form.diminished-investment", "form.freeourinternet", "form.fwact", "form.tpa",
+        "bot.best_cop", "bot.economics_free_zone", "bot.corrupt_result", "bot.recursive"]
+}
+
+clusters = {
+    'positive': [
+        "1070716784137", "10707215368462", "10707899924781", "1070776003187",
+        "10711874306044", "10707195721037", "1071141320641", "1070509290734",
+        "10705304876386", "1070582875194"],
+    'negative': [],
 }
