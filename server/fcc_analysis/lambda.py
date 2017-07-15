423f3c16e3d094f91f5a70fc837f7a571280f4b9
Kimberly Nicholls <kielni@yahoo.com>
2017-07-14 17:48:59 -0700

    write results to S3 bucket set in env

--- server/fcc_analysis/lambda.py
+++ server/fcc_analysis/lambda.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
 import json
 import os
 
+import boto3
 from elasticsearch import Elasticsearch
 
 import tags
@@ -24,23 +26,27 @@ def query_by_source(event=None, context=None):
       "size": 0,
       "query": {
         "bool": {
-          "should": [
-            {
-              "term": {
-                "analysis.sentiment_manual": True
-              }
-            },
-            {
-              "term": {
-                "analysis.titleii": True
-              }
-            },
-            {
-              "term": {
-                "analysis.sentiment_sig_terms_ordered": True
-              }
+          "filter": {
+            "bool": {
+              "should": [
+                {
+                  "term": {
+                    "analysis.sentiment_manual": True
+                  }
+                },
+                {
+                  "term": {
+                    "analysis.titleii": True
+                  }
+                },
+                {
+                  "term": {
+                    "analysis.sentiment_sig_terms_ordered": True
+                  }
+                }
+              ]
             }
-          ]
+          }
         }
       },
       "aggs": {
@@ -56,9 +62,13 @@ def query_by_source(event=None, context=None):
         }
       }
     }
-    query['query']['bool']['should'].append({
+    query['query']['bool']['filter']['bool']['should'].append({
         'terms': {'analysis.source': tags.sources['positive']}
     })
+    query['query']['bool']['filter']['bool']['should'].append({
+        'terms': {'analysis.more_like_this.src_doc_id': tags.clusters['positive']}
+    })
+    print('positive query=%s' % json.dumps(query))
     resp = es.search(index='fcc-comments', body=query)
     by_source = {}
     for src in resp['aggregations']['source']['buckets']:
@@ -108,6 +118,7 @@ def query_by_source(event=None, context=None):
     query['query']['bool']['should'].append({
         'terms': {'analysis.source': tags.sources['negative']}
     })
+    print('negative query=%s' % json.dumps(query))
     resp = es.search(index='fcc-comments', body=query)
     by_source = {}
     for src in resp['aggregations']['source']['buckets']:
@@ -120,6 +131,17 @@ def query_by_source(event=None, context=None):
     })
     return rval
 
+
+def query_by_source_s3(event=None, context=None):
+    data = query_by_source(event, context)
+    # save output to S3
+    s3 = boto3.resource('s3')
+    s3.Object(os.environ['S3_BUCKET'], 'by_source.json').put(
+      Body=json.dumps(data),
+      ContentType='application/json',
+      Expires=(datetime.now() + timedelta(hours=6))
+    )
+    return data
 
 if __name__ == '__main__':
     print(json.dumps(query_by_source(), indent=2))
